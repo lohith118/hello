9e2ab7e7ab14d3ce476502a9c9a975c41b556f93
lohith118 <52988905+lohith118@users.noreply.github.com>
2019-07-18 12:38:39 +0530

    Update and rename tt.py to pt.py

---
+++ pt.py
@@ -0,0 +1,5 @@
+n=int(input("enter the number"))
+b=1
+for i in range(0,n+1):
+    b=b*i
+print(b)    
--- tt.py
+++
@@ -1,6 +0,0 @@
-n=int(input("enter the number"))
-b=0
-for i in range(0,n):
-    a=int(input(""))
-    b=b+a
-print(b)
